d89b780cc8320e45e39c7dccb3e8f14d72593d8c
Nguyễn Nghiệp <nghiep2k3>
2024-07-11 15:38:21 +0700

    code

--- FE/src/page/BestSell/Bestsell.jsx
+++ FE/src/page/BestSell/Bestsell.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Cart from '../../components/Cart/Cart'
 export default function Bestsell() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <div>
             Bestsell
